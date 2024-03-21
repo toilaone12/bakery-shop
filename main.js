2913afa0b4b01c3146eff4cb0057b61d653860bb
Bao Son <toilaone12@gmail.com>
2024-03-21 15:10:13 +0700

    second upload

--- main.js
+++ main.js
@@ -10,4 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
             header.classList.remove('header-fixed');
         }
     });
+    document.getElementById("name").addEventListener("change", function() {
+        let name = document.getElementById("name");
+        var nameError = document.getElementById("name-error");
+        if(name.value.length == 0){
+            nameError.innerHTML = "Vui lòng nhập tên của bạn";
+        }
+    });
+
+    document.getElementById("email").addEventListener("change", function() {
+        let email = document.getElementById("email");
+        var emailError = document.getElementById("email-error");
+        if(email.value.length == 0){
+            emailError.innerHTML = "Vui lòng nhập email của bạn";
+        }
+    });
+
+    document.getElementById("phone").addEventListener("change", function() {
+        let phone = document.getElementById("phone");
+        var phoneError = document.getElementById("phone-error");
+        if(phone.value.length == 0){
+            phoneError.innerHTML = "Vui lòng nhập số điện thoại của bạn";
+        }
+    });
 })
