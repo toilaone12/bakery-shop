dd8b8eebfd23b241a8b399b93b3d0af2d08830df
BaoSon <toilaone12@gmail.com>
2024-03-27 23:16:19 +0700

    27/3

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+function htmlRecipe(data,id){
+    // console.log();
+    let html = ``;
+    html += `<div class="modal-content">`;
+    html += `<div class="modal-header">`;
+    html += `<h3 class="modal-header__title">Thông tin công thức</h3>`;
+    html += `<div class="modal-header__icon">`;
+    html += `<i class="fa-solid fa-xmark"></i>`;
+    html += `</div>`;
+    html += `</div>`;
+    html += `<div class="modal-body">`;
+    html += `<div class="row">`;
+    html += `<div class="col-4">`;
+    html += `<img src="${data[id].image}" class="modal-image" alt="">`;
+    html += `</div>`;
+    html += `<div class="col-8">`;
+    html += `<h2 class="modal-body__title">${data[id].title}</h2>`;
+    html += `<h4 class="modal-body__summary">${data[id].summary}</h4>`;
+    html += `<div class="modal-body__recipe">`;
+    html += `<p class="modal-body__recipe-title">Nguyên liệu bao gồm:</p>`;
+    html += `<ul class="modal-body__recipe-component">`;
+    for(let i = 0; i < data[id].recipes.length; i++){
+        html += `<li class="modal-body__recipe-items">${data[id].recipes[i]}</li>`;
+    }
+    html += `</ul>`;
+    html += `</div>`;
+    html += `<div class="modal-body__tutorial">`;
+    html += `<p class="modal-body__tutorial-title">Cách làm:</p>`;
+    html += `<ul class="modal-body__tutorial-component">`;
+    for(let i = 0; i < data[id].tutorials.length; i++){
+    html += `<li class="modal-body__tutorial-items">${data[id].tutorials[i]}</li>`;
+    }
+    html += `</ul>`;
+    html += `</div>`;
+    html += `</div>`;
+    html += `</div>`;
+    html += `</div>`;
+    html += `</div>`;
+    return html;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
         var headerWrapper = document.querySelector('.header-wrapper');
@@ -17,15 +58,17 @@ document.addEventListener('DOMContentLoaded', function() {
     let chooseRecipe = document.querySelectorAll('.choose-recipe');
     chooseRecipe.forEach(function (choose) {
         choose.addEventListener("click", function() {
+            let id = choose.getAttribute('data-id');
+            // console.log(recipes);    
+            modal.innerHTML = htmlRecipe(recipesItems,id - 1);
             modal.classList.add('show');
+            let close = document.querySelector('.modal-header__icon');
+            close.addEventListener("click", function() {
+                modal.classList.remove('show');
+            });
         })
     })
 
-    let close = document.querySelector('.modal-header__icon');
-    close.addEventListener("click", function() {
-        // let modal = document.getElementById("myModal");
-        modal.classList.remove('show');
-    });
 
     window.onclick = function(event) {
         if (event.target == modal) {
---
+++ recipe-items.js
@@ -0,0 +1,183 @@
+let recipesItems = [
+    {
+        id: 1,
+        image: 'https://bepcuana.com/wp-content/uploads/2024/01/cach-lam-mut-mit-deo.jpg',
+        title: 'Cách làm mứt mít dẻo ngào đường cực dễ tại nhà',
+        summary: 'Với các loại mứt ngày tết như mứt gừng, mứt chuối, mứt hạt sen,… có thể không còn lạ miệng nữa và bạn đang tìm kiếm một loại mứt mới vừa là miệng mà vừa đảm bảo sức khỏe thì bạn có thể tham khảo ngay cách làm mứt mít dẻo ngào đường thơm lừng. Chỉ nhắc đến mít thì chắc hẳn ai cũng thích bởi hương thơm ngào ngạt cùng với hương vị ngọt thanh, thịt mít giòn giòn sựt sựt ăn hoài mà không chán, giờ đây lại tạo thành món mứt nữa thì chắc hẳn sẽ là món mứt mít đắc khách nhất ngày tết.',
+        recipes: [
+            'Mít: 500gr',
+            'Đường: 200gr',
+            'Chanh: ½ quả',
+            'Muối: ¼ thìa cafe',
+        ],
+        tutorials: [
+            'Bước 1: Mít bạn mua về bạn tách bỏ phần sơ bỏ hạt và bổ đôi miếng mít cho vừa ăn, nếu bạn muốn ăn miếng to thì bạn có thể để nguyên.',
+            'Bước 2: Cho 200g đường và nước cốt chanh vào tô sạch, sau đó cho mít vào trộn đều. Đợi khoảng 30 phút cho đường tan hết rồi trộn với mít.',
+            'Bước 3: Đặt chảo lên bếp và để lửa vừa. Bạn nên sử dụng chảo chống dính để mít không bị dính hoặc cháy trong quá trình nấu. Cho nước đường ướp mít vào chảo trước, đun nóng đến khi đường sủi bọt, vàng và đặc lại thì cho thịt mít vào. Khuấy nhẹ nhàng để đường thấm vào mít khoảng 10 phút – 15 phút cho đến khi đường dẻo, cứng lại và dính vào mít thì tắt bếp.',
+            'Bước 4: Mứt mít sau khi sên xong bạn cho mít ra khay để phơi nắng, bạn canh có nắng to thì phơi trong khoảng 3 đến 5 tiếng để mít khô lại. Nếu không có nắng, bạn có thể dùng lò nướng để sấy mít trong khoảng 1 giờ với nhiệt độ 100-130 độ C để mít khô. Nếu bạn phơi nắng, bạn nên dùng màng lưới che chắn lại kẻo có côn trùng đậu vào thì ảnh hưởng đến sức khỏe.',
+        ]
+    },
+    {
+        id: 2,
+        image: 'https://bepcuana.com/wp-content/uploads/2020/09/cach-lam-banh-bao.jpg',
+        title: 'Cách làm bánh bao nhân thịt và bánh bao chay tại nhà',
+        summary: 'Cách làm bánh bao vừa thơm ngon vừa dể làm thích hợp với mọi hoàn cảnh, có thể dùng cho bữa sáng hay bất kể lúc nào trong ngày cũng đều đem lại hương vị thơm ngon và hấp dẫn. Vỏ bánh trắng ngần hòa cùng nhân thịt nạc, trứng cút, nắm mèo đạm đà hương vị bên trong đã khiến bánh bao trở thành món ăn khoái khẩu của nhiều người. Nếu bạn muốn làm bánh này thì hãy vào bếp cùng với Bếp Của Na thực hiện ngay công thức làm bánh ngay bây giờ nhé.',
+        recipes: [
+            'Bột mì: 300g',
+            'Men nở:10g',
+            'Đường: 30g',
+            'Muối 15g',
+            'Nước lọc (hoặc sữa): 150ml',
+            'Thịt lợn: 250g',
+            'Trứng cút: 10 quả',
+            'Hành tím khô: 10 củ',
+            'Hành tây: ¼ củ',
+            'Mộc nhĩ ( nấm hương ): 5 cái',
+            'Lạp xưởng: 1 thanh',
+            'Hạt nêm, đường, tiêu: 1tbsp',
+            'Muối: ½ tbsp',
+        ],
+        tutorials: [
+            'Bước 1: Phần bột bánh: </br> Đầu tiên cho bột mì vào một cái tô lớn, sau đó lấy men khô, đường trộn đều với sữa rồi ủ khoảng 20 phút để men kích hoạt </br> Tiếp theo, bạn cho muối vào tô bột rồi đổ hết phần men đã ủ vào trong tô trộn đều lên rồi dùng tay nhào bột nhiều lần cho đến khi thành một khối bột mịn </br> Nếu trường hợp bột dính tay, bạn nên cho thêm ít bột vào tránh cho quá nhiều bột sẽ bị khô. Sau đó, cho bột trở vô lại tô, dùng một cái khăn ẩm hay màng bọc thực phẩm bọc lại ủ bánh trong vòng 1 tiếng đến khi bột nở gấp đôi là được',
+            'Bước 2: Làm phần nhân bánh: </br> Trong thời gian ủ bột thì bạn đi làm phần nhân bánh, trước tiên đem trứng cút đi luộc chín và lột sạch vỏ </br> Bạn đem hành tây, mộc nhĩ đã ngâm nở xắt nhuyễn. Hành khô bạn đem đi thái lát và phi vàng </br> Phần thịt rửa sạch xong thái mỏng rồi băm nhuyễn (có thể dùng máy xay thịt nếu có), lạp xưởng thì thái lát mỏng </br> Cho phần thịt băm vào tô trộn đều với cái nguyên liêu và gia vị lại với nhau cho thắm vị khoảng 15 phút. Sau đó múc một cục nhân vừa đủ, cho lạp xưởng và trứng cút vào giữa túm lại tạo thành những viên nhân tròn.',
+            'Bước 3: Nặn bánh: Sau khi ủ bột xong, mang ra và nhào thêm 3- 5 phút nữa rồi chia bột ra thành những phần bằng nhau. </br> Lấy từng phần bột ra cán mỏng, xếp nhân vào giữa rồi túm mép lại cho kín miệng bánh. </br> Cứ thế lần lượt làm cho hết rồi để bánh vào sửng hấp có lót một lớp giấy nến dưới từng chiếc bánh, tiếp tục ủ bánh thêm 20 phút nữa rồi mới đem đi hấp.',
+            'Bước 4: Hấp bánh: Lấy nồi hấp đun nước cho thật sôi, thêm 2 thìa giấm vào nước để hấp bánh được trắng hơn. Cho bánh vào hấp cách thủy khoảng 18-20 phút là được </br> Bánh bao làm xong thơm ngon đến khó tả với vỏ bánh mềm xốp hòa với nhân thịt bùi béo đậm đà, là một món ăn thích hợp với mọi hoàn cảnh đảm bảo đầy đủ chất dinh dưỡng và sức khỏe cho gia đinh',
+        ]
+    },
+    {
+        id: 3,
+        image: 'https://bepcuana.com/wp-content/uploads/2020/09/cach-lam-banh-flan-truyen-thong.jpg',
+        title: 'Cách làm bánh flan SIÊU ĐƠN GIẢN tại nhà',
+        summary: 'Cách làm bánh flan thơm ngon, mềm mịn tại nhà cũng những nguyên liệu vô cùng đơn giản dễ dàng làm ra một món tráng miệng phổ biến được nhiều người ưa thích. Với vị ngọt hòa huyện với độ béo nhẹ đã làm nên một hương vị đặc trưng cho bánh flan ngon. Sau đây Bếp Của Na sẽ gửi đến cho bạn một vài công thức làm bánh để thực hiện làm bánh flan đã được biến tấu từ bánh truyền thống giúp bạn có nhiều sự lựa chọn vào một mùa hè ôi bức như thế này.',
+        recipes: [
+            `6 quả trứng gà`,
+            `250ml sữa tươi`,
+            `1 hộp sữa đặc có đường`,
+            `100g đường`,
+            `Vani 2 ống`,
+            `Nước lọc`,
+            `1/2 quả chanh`,
+            `Khuôn đựng (cốc hoặc hộp)`,
+        ],
+        tutorials: [
+            'Bước 1: Cách làm caramel: Cho 70g đường với 100ml nước lọc, cho lên bếp đun lửa vừa từ 5 – 7 phút khi thấy hỗn hợp sôi thì vắt nửa lát chanh vào và đun đến khi hỗn hợp chuyển sang màu cánh gián là được (chú ý không nên khuấy trong lúc nấu vì như thế sẽ bị lại đường ). </br> Sau đó cho hỗn hợp ra cốc hoặc khuôn tráng mỏng một lớp đường rồi để vào ngăn mát tủ lạnh.',
+            'Bước 2: Cách làm bánh flan: Tách lấy 4 lòng đỏ trứng gà, và 2 quả trứng lấy cả lòng trắng và lòng đỏ, cho thêm 30g đường và 2 ống vani vào khuấy nhẹ theo một chiều ( không được đánh bông ) cho đến khi đường tan hòa huyện vào nhau </br> Đổ sữa tươi vào nồi đun lửa nhỏ cho đển khi sữa sôi tiêm rồi tắt bếp ( không được để sôi lớn vì như thế sẽ làm cho trứng bị chín ) cho sữa đặc vào khuấy điều </br> Cho từ từ sữa vào hỗn hợp trứng, vừa đổ vào vừa khuấy theo một chiều cho huyện vào nhau có màu vàng nhạt. Sau đó lọc hỗn hợp qua một cái rây 1 đến 2 lần để cho hỗn hợp được mịn hơn </br> Múc hỗn hợp trứng sữa vào cốc đã tráng một lớp caramen, đổ đầy cốc và đậy nắp lại. </br> Sắp các cốc bánh vào xửng hấp và mang đi hấp cách thủy từ 30 – 40 phút. Hấp với lửa nhỏ nhất cứ 10 phút thì mở nắp một lần để nước không đọng lại trên mặt bánh (có thể hấp bánh bằng nồi cơm điện ) </br> Để kiểm tra bánh chín chưa chỉ cần lấy cây tăm cắm xuống nếu thấy không dính thì đã chín. Nhắc nồi xuống lấy bánh ra mở nắp và để bánh thật nguội rồi mới cho vào ngăn mát tủ lạnh.',
+        ]
+    },
+    {
+        id: 4,
+        image: 'https://bepcuana.com/wp-content/uploads/2023/06/cach-lam-banh-trung-thu-ngan-lop.jpg',
+        title: 'Cách làm bánh trung thu ngàn lớp Đài Loan lạ đẹp mắt',
+        summary: 'Cách làm bánh trung thu ngàn lớp đơn giản tại nhà mà không mất quá nhiều thời gian. Một mùa trung thu nữa lại đến nhiều người tấp nập đua nhau để làm ra những mẻ bánh trung thu thật ngon. Nào hãy cùng vào bếp làm món bánh trung thu ngàn lớp với Bếp Của Na nhé.',
+        recipes: [
+            `Khoai môn: 400g`,
+            `Muối: 2g`,
+            `Nước 200ml`,
+            `Đường: 100g`,
+            `Nước: 100ml`,
+            `Sữa đặc: 100g`,
+            `Tinh bột bắp: 10g`,
+            `Nước: 30ml`,
+            `Tinh chất khoai môn: 1 ít`,
+            `Dầu dừa: 40g`,
+            `Lòng đỏ trứng muối: 12 cái`,
+            `Rượu trắng: 50ml`,
+            `Nước: 110ml`,
+            `Đường: 40g`,
+            `Muối: 3g`,
+            `Bơ lạt: 70g`,
+            `Bột mì đa dụng: 200g`,
+            `Phần bột dầu`,
+            `Bột mì đa dung: 130g`,
+            `Dầu ăn: 55g`,
+            `Lòng đỏ: 1 cái`,
+            `Mè: một ít`,
+        ],
+        tutorials: [
+            'Bước 1: Phần nhân bánh: Khoai môn mua về bạn sơ chế cho sạch rồi cắt thành những miếng nhỏ để nấu cho mau mềm. '+
+            '</br> Cho khoai vào nồi, thêm vào 200ml nước và 2 g muối vào. Bắt lên bếp nấu lửa trung bình khoảng 8 – 10 phút cho khoai chín mềm. '+
+            '</br>Nhắc khỏi bếp rồi đậy kín lại. Trong lúc này thì bạn đi nấu nước đường. Cho vào chảo 100ml nước và 100g đường, bắt lên bếp đun cho tan đường. </br>'+
+            'Sau đó cho nước đường vừa nấu và 100g sữa đặc vào nồi khoai môn hấp rồi dùng máy xay xay nhuyễn.</br>'+ 
+            'Lượt hỗn hợp khoai qua rây cho vào một cái nồi khác. Pha bột bắp với nước, khuấy tan rồi cho lại vào chảo nhân, khuấy đều hỗn hợp lên. Nếu muốn màu đậm hơn thì cho thêm một ít tinh chất vanila vào.' +
+            '</br>Bắt nồi nhân lên bếp sên với lửa lớn, khuấy liên tục cho đều tay. Đến khi thấy sôi thì hạ xuống lửa trung bình và vẫn khuấy đều tay. '+
+            '</br>Lúc này bạn cho ½ lượng dầu dừa vào khuấy cho hòa quyện rồi mới cho tiếp phần còn lại vào. Như vậy phần nhân mới không bị tách dầu. Và tiếp tục khuấy đều khoảng 20 phút cho đến khi nhân khô dẻo, sờ thử tay vào mà không bị dính tay thì đạt.'+
+            '</br>Cho nhân ra một cái bát, đậy kín và để nguội.'+
+            '</br>Tiếp theo thì đi hấp trứng muối. Bạn rửa trứng muối với rượu trắng rồi cho vào xửng đem đi hấp hoặc có thể nướng. Thời gian hấp khoảng 10 phút.'+
+            '</br>Sau đó bạn đi chia nhân thành 12 phần bằng nhau khoảng 45g cho 1 viên. Rồi cho trứng muối vào giữa, vo lại cho tròn.',
+            'Bước 2: Phần bột nước: '+
+            '</br>Chuẩn bị một cái bát, cho nước, muối, đường và bơ melt chảy khuấy đều cho tan hết đường. Sau đó cho bột vào và trộn đều bột lên cho chất lỏng được thấm vào bột.'+
+            '</br>Dùng màng bọc đậy kín và để bột nghỉ 15 phút. Sau 15 phút thì đem bột ra nhồi và đập bột, với cách này một sẽ nhanh mịn và ngon. Tiếp tục để bột nghỉ khoảng 1 giờ.',
+            'Bước 3: Phần bột dầu: '+
+            '</br>Cho vào bát 55g dầu và 130g bột mì trộn đều cho dầu và bột đều với nhau. Sau đó chia bột thành 12 phần bằng nhau khoảng 15g.',
+            'Bước 4: Chia bột và gói bánh'+
+            '</br>Bột nước sau khi nghĩ xong thì bạn đem ra chia thành 12 phần bằng nhau, khoảng 35g/ viên. Vo tròn viên bột lại và bắt đầu đi cán.'+
+            '</br>Viên nào vo trước thì cán trước. Ấn dẹp viên bột nước rồi cho viên bột dầu vào giữa, túm bột lại sao cho kín mí bột là được. Lần lượt làm cho hết phần bột còn lại rồi mới đi cán.'+
+            '</br>Cho viên bột đầu tiên ra bàn, dùng cây cán bột cán theo chiều dọc và độ dày khoảng 1 – 2mm.'+
+            '</br>Sau đó, gấp miếng bột lại làm 3 và xoay miếng bột lại tiếp tục cán tương tự và gấp làm 3 một lần nữa. Vậy là bột của chúng ta đã có chín lớp và bạn tiếp tục làm cho hết bột.'+
+            '</br>Khi cán bột xong thì để bột nghỉ 15 phút để bột không bị co giãn khi cán.'+
+            '</br>Sau 15 phút cho viên bột ra bàn cán mỏng rồi cho viên nhân vào giữa, túm bột lại cho tròn đều.'+
+            '</br>Cho bánh lên khay rồi phết mỏng một lớp trứng gà lên mặt bánh. Sau đó bạn cho một ít mè đen lên trên mặt bánh rồi đem nướng.',
+            'Bước 5: Nướng bánh: '+
+            '</br>Làm nóng lò ở nhiệt độ khoảng 190 độ C 15 phút.'+
+            '</br>Cho bánh vào nướng khoang 20 – 22 phút, 190 độ C tùy vào nhiệt độ lò của từng nhà.'+
+            '</br>Thấy mặt bánh hơi vàng thì lấy ra và để nguội. Bánh cho vào hộp kín thì có thể bảo quản được 4 – 5 ngày với nhiệt độ thường. Hoặc có thể cho vào ngăn mát tủ lạnh, khi ăn chỉ cần hâm nóng lại là dùng được.',
+        ]
+    },
+    {
+        id: 5,
+        image: 'https://bepcuana.com/wp-content/uploads/2020/10/cach-lam-banh-flan-rau-cau-trai-dua-300x200.jpg',
+        title: 'Cách Làm Bánh Flan Rau Câu Trái Dừa',
+        summary: 'Cách làm bánh flan rau câu trái dừa là sự kết hợp thật mới mẻ của thạch lá dứa và bánh flan. Tất cả các nguyên liệu hòa quyện vào nhau đã làm nên một món ăn độc đáo và tươi mát thích hợp cho mùa hè và đặt biệt vào những ngày cuối tuần bạn có thể làm cho gia đình và bạn bè cùng thưởng thức món tráng miệng này thật thơm ngon và bổ dưỡng. Bếp Của Na sẽ gửi đến bạn công thức làm bánh cực kì đơn giản và để làm để có một món tráng miệng thơm ngon tại nhà nhé!',
+        recipes: [
+            `Dừa xiêm: 1 quả ( cơm dừa không quá già hay quá mềm)`,
+            `Trứng gà: 2 quả`,
+            `Sữa tươi: 400ml`,
+            `Lá dứa: 10g`,
+            `Đường cát: 40g`,
+            `Bột rau câu: 25g`,
+            `Vani: 15g`
+        ],
+        tutorials: [
+            'Bước 1: Làm phần rau câu bên trong trái dừa'+
+            '</br>Dùng dao chặt bỏ phần vỏ cứng bên ngoài vỏ dừa, gọt sạch vỏ lụa rồi rửa lại cho trái dừa được trắng. Tiếp đến, bạn khoét một lỗ nhỏ trên đầu trái dừa với đường kính khoảng 2cm, sau đó đổ hết phần nước dừa bên trong ra ngoài.'+
+            '</br>Lá dứa rửa sạch, cắt khúc, cho vào máy xay sinh tố thêm một it nước, xay nhuyễn. Lọc lá dứa đã xay ra một cái rây để lấy nước cốt.'+
+            '</br>Bắt một cái nồi lên bếp, lần lượt cho nước cốt lá dứa và nước dừa vào bắt lên bếp đun sôi, cho 20g đường và 20g bột rau câu vào một cái bát trộn đều, rồi cho từ từ vào nước lá dứa đang sôi vừa cho vừa khuấy đều tay cho hỗn hợp hòa tan. Đến khi sôi lên lại thì tắt bếp đổ vào khuôn và để nguội, khi rau câu đã đông cứng thì cho vào ngăn mát tủ lạnh',
+            'Bước 2: Làm flan trứng'+
+            '</br>Lấy một chiếc âu, cho sữa tươi, 5g bột rau câu đường, 20g đường và trứng vào khuấy đều cho hỗn hợp quyện vào nhau, sau đó lọc hỗn hợp qua một cái rây để cho bánh được mịn hơn.'+
+            '</br>Cho hỗn hợp lên bếp thêm vani vào (không có cũng không sao) đun đến khi thấy sôi lăn tăn thì tắt bếp.'+
+            'Bước 3: Hấp bánh flan rau câu trái dừa',
+        ]
+    },
+    {
+        id: 6,
+        image: 'https://bepcuana.com/wp-content/uploads/2022/01/cach-lam-banh-xeo-gion-300x200.jpg',
+        title: 'Cách làm bánh xèo ngon giòn lâu chuẩn vị',
+        summary: 'Bánh xèo là món ăn quen thuộc dân dã của người miền Tây, đây là món ăn mà mỗi lần gia đình có tụ họp lại hay những ngày lễ thì thường quây quần lại với nhau làm món bánh xèo này. Món bánh vừa ngon, nguyên liệu thì đơn giản rất dễ làm, cùng vào bếp với Bếp Của Na thực hiện cách làm bánh xèo này nhé.',
+        recipes: [
+            `Bột bánh xèo pha sẵn: 1 bịch`,
+            `Bia: 100ml`,
+            `Thịt heo: 500g`,
+            `Tôm: 500g`,
+            `Sắn: 2 củ`,
+            `Hành lá: 200g`,
+            `Giá đỗ: 500g`,
+            `Cà rốt: 1 củ`,
+            `Tỏi, ớt: tùy ý`,
+            `Rau ăn kèm: xà lách, rau diếp cá, húng quế, cải bẹ xanh….`,
+            `Gia vị thông dụng: muối, hạt nêm, nước mắm, đường….`,
+        ],
+        tutorials: [
+            'Bước 1: Sơ chế các nguyên liệu'+
+            '</br>Thịt, tôm mua về bạn làm sạch rửa qua với nước muối pha loãng rồi rửa lại với nước sạch, để ráo. Thịt thì bạn cắt thành những lát mỏng, tôm thì cắt bỏ râu có thể để nguyên hoặc lột vỏ.'+
+            '</br>Sắn thì gọt vỏ, rửa sạch rồi thái thành từng sợi, cà rốt thì bạn cũng gọt vỏ rồi dùng bàn bào bào thành từng sợi mỏng để làm nước chấm.'+
+            '</br>Rau ăn kèm, giá đỗ, hành lá thì bạn rửa qua nước muối pha loãng rồi rửa lại với nước sạch, để riêng cho ráo. Hành lá thì bạn cắt khúc khoảng 2 cm.',
+            'Bước 2: Pha bột và xào nhân'+
+            '</br>Cho bột vào một cái thao, cho bia và nước vào khuấy đều cho bột tan đều rồi để bột nghỉ khoảng 30 phútn.'+
+            '</br>Bắt một cái chảo lên bếp, cho thịt vào xào săn lại nêm thêm 1 mcf hạt nêm đảo đều cho thịt chín khoảng 80% rồi cho thịt ra bát. Còn tôm thì nhanh chín nên bạn không cần xào.'+
+            'Bước 3: Hấp bánh flan rau câu trái dừa'+
+            '</br>Bạn trộn giá, củ sắn cắt sợi, hành lá cắt khúc lại với nhau..'+
+            '</br>Sau đó bắt một cái chảo lên bếp, cho dầu ăn vào, cho tôm, và thịt vào đảo sơ qua rồi cho một vá bột vào tráng đều quanh chảo rồi cho một ít hành lá, sắn và giá đỗ lên trên mặt, đậy nắp lại cho bánh chín vàng giòn.'+
+            '</br>Khi bánh chín vàng thì bạn gấp đôi bánh lại rồi cho ra dĩa. Tiếp tục làm hết phần bột và nhân còn lại cho đến hết.',
+        ]
+    }
+]
