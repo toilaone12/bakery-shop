e8d9f5d95e6abf590586da86cc79862d0708a91c
BaoSon <toilaone12@gmail.com>
2024-03-29 00:32:25 +0700

    29/3

--- main.js
+++ main.js
@@ -39,7 +39,71 @@ function htmlRecipe(data,id){
     return html;
 }
 
+function htmlTrick(data,id){
+    // console.log();
+    let html = ``;
+    html += `<div class="modal-content">`;
+    html += `<div class="modal-header">`;
+    html += `<h3 class="modal-header__title">Thông tin công thức</h3>`;
+    html += `<div class="modal-header__icon">`;
+    html += `<i class="fa-solid fa-xmark"></i>`;
+    html += `</div>`;
+    html += `</div>`;
+    html += `<div class="modal-body">`;
+    html += `<div class="row">`;
+    html += `<div class="col-4">`;
+    html += `<img src="${data[id].image}" class="modal-image" alt="">`;
+    html += `</div>`;
+    html += `<div class="col-8">`;
+    html += `<h2 class="modal-body__title">${data[id].title}</h2>`;
+    html += `<h4 class="modal-body__summary">${data[id].summary}</h4>`;
+    if(data[id].recipe.title.length !== 0){
+    html += `<div class="modal-body__recipe">`;
+    html += `<p class="modal-body__recipe-title">${data[id].recipe.title}</p>`;
+    html += `<ul class="modal-body__recipe-component">`;
+    for(let i = 0; i < data[id].recipe.detail.length; i++){
+        html += `<li class="modal-body__recipe-items">${data[id].recipe.detail[i]}</li>`;
+    }
+    html += `</ul>`;
+    html += `</div>`;
+    }
+    if(data[id].cause.title.length !== 0){
+    html += `<div class="modal-body__cause">`;
+    html += `<p class="modal-body__cause-title">${data[id].cause.title}</p>`;
+    html += `<ul class="modal-body__cause-component">`;
+    for(let i = 0; i < data[id].cause.detail.length; i++){
+    html += `<li class="modal-body__cause-items">${data[id].cause.detail[i]}</li>`;
+    }
+    html += `</ul>`;
+    html += `</div>`;
+    }
+    if(data[id].fix.title.length !== 0){
+    html += `<div class="modal-body__fix">`;
+    html += `<p class="modal-body__fix-title">${data[id].fix.title}</p>`;
+    html += `<ul class="modal-body__fix-component">`;
+    for(let i = 0; i < data[id].fix.detail.length; i++){
+    html += `<li class="modal-body__fix-items">${data[id].fix.detail[i]}</li>`;
+    }
+    html += `</ul>`;
+    html += `</div>`;
+    }
+    if(data[id].note.title.length !== 0){
+    html += `<div class="modal-body__note">`;
+    html += `<p class="modal-body__note-title">${data[id].note.title}</p>`;
+    html += `<ul class="modal-body__note-component">`;
+    for(let i = 0; i < data[id].note.detail.length; i++){
+    html += `<li class="modal-body__note-items">${data[id].note.detail[i]}</li>`;
+    }
+    html += `</ul>`;
+    html += `</div>`;
+    }
+    html += `</div>`;
+    html += `</div>`;
+    return html;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+    //lan chuot keo header xuong
     window.addEventListener('scroll', function() {
         var headerWrapper = document.querySelector('.header-wrapper');
         var header = document.querySelector('.header'); 
@@ -54,6 +118,7 @@ document.addEventListener('DOMContentLoaded', function() {
             header.removeAttribute('style');
         }
     });
+    //modal nguyen lieu
     let modal = document.getElementById("myModal");
     let chooseRecipe = document.querySelectorAll('.choose-recipe');
     chooseRecipe.forEach(function (choose) {
@@ -68,14 +133,30 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         })
     })
-
-
+    //modal meo nau an
+    let modalTrick = document.getElementById('modalTrick');
+    let chooseTrick = document.querySelectorAll('.choose-trick');
+    chooseTrick.forEach(function (choose) {
+        choose.addEventListener("click", function() {
+            let id = choose.getAttribute('data-id');
+            // console.log(recipes);    
+            modalTrick.innerHTML = htmlTrick(tricksItem,id - 1);
+            modalTrick.classList.add('show');
+            let close = document.querySelector('.modal-header__icon');
+            close.addEventListener("click", function() {
+                modalTrick.classList.remove('show');
+            });
+        })
+    })
+    //click ra ngoai modal
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.classList.remove('show');
+        }else if(event.target == modalTrick){
+            modalTrick.classList.remove('show');
         }
     }
-
+    //ktra ten
     document.getElementById("name").addEventListener("change", function() {
         let name = document.getElementById("name");
         var nameError = document.getElementById("name-error");
@@ -83,7 +164,7 @@ document.addEventListener('DOMContentLoaded', function() {
             nameError.innerHTML = "Vui lòng nhập tên của bạn";
         }
     });
-
+    //ktra email
     document.getElementById("email").addEventListener("change", function() {
         let email = document.getElementById("email");
         var emailError = document.getElementById("email-error");
@@ -91,7 +172,7 @@ document.addEventListener('DOMContentLoaded', function() {
             emailError.innerHTML = "Vui lòng nhập email của bạn";
         }
     });
-
+    //ktra so dien thoai
     document.getElementById("phone").addEventListener("change", function() {
         let phone = document.getElementById("phone");
         var phoneError = document.getElementById("phone-error");
---
+++ trick-items.js
@@ -0,0 +1,154 @@
+let tricksItem = [
+    {
+        id: 1,
+        image: 'https://bepcuana.com/wp-content/uploads/2020/11/meo-lam-banh-flan-khong-bi-ro-300x200.jpg',
+        title: 'Mẹo làm bánh flan không bị rỗ',
+        summary: 'Như các bạn cũng biết BÁNH FLAN là một trong những món tráng miệng phổ biến hiện nay vừa ngon miệng thanh mát mà còn có đủ chất dinh dưỡng thích hợp cho mọi lứa tuổi. Tuy rất dễ làm nhưng nhiều người lại mắc sai lầm nên bánh flan làm ra bị rỗ không còn được mềm mịn nên có vẻ không được đẹp mắt. Bởi thế nhìn thì có vẻ đơn giản nhưng chưa chắc chị em nào cũng làm được đâu nên hôm nay Bếp Của Na sẽ chia bạn một số cách mẹo làm bánh flan không bị rỗ để bạn làm ra được mềm mịn và thơm ngon.',
+        recipe: {
+            title: 'Lựa chọn nguyên liệu làm bánh flan',
+            detail: [
+                'Trứng gà: thường trứng gà sẽ có 2 loại là gà ta và gà công nghiệp, thông thường thì trứng gà công nghiệp được sử dụng trong việc làm bánh nhiều hơn vì loại trứng to, dễ mua ở các cửa hàng, siêu thị và giá thành lại vừa rẻ thích hợp để sử dụng làm bánh. Còn trứng gà ta thì có kết cấu nhỏ hơn gà công nghiệp những phần lòng đỏ thì lại lớn và có màu vàng đậm hơn, nên sẽ cho ra thành phẩm có mùi vị thơm ngon hơn và ít bị tanh mùi trứng hơn, tuy nhiên trứng gà ta sẽ mắc hơn trứng công nghiệp. Dù là trứng công nghiệp hay trứng gà ta thì bánh làm ra vẫn có hương vị như nhau.',
+                'Sữa tươi: sữa thì bạn nên chọn những loại có nhiều độ béo và không có đường vì đa phần nếu sử dụng sữa không đường làm bánh thì sẽ giữ được đúng hương vị của món bánh và bánh làm ra sẽ thơm ngon béo ngậy. Sữa tươi có đường vẫn có thể dùng làm bánh nhưng sẽ không ngon bằng sữa không đường vì thế nên cân nhắc khi lựa chọn.',
+                'Vani: giúp làm giảm mùi tanh của trứng và làm cho bánh flan được thơm ngon hơn.',
+                'Whipping cream: giúp cho bánh flan được thơm và béo hơn đồng thời làm cho bánh mềm mịn ( nếu không có cũng không sao có thể thay bằng sữa đặc)',
+            ]
+        },
+        cause: {
+            title: 'Nguyên nhân bánh flan bị rỗ đáy',
+            detail: [
+                'Bên cạnh bánh hay bị rỗ mặt thì cũng có trường hợp là bánh bị rỗ phần đáy và bên trong bánh thường thì bị cả 2 trường hợp đó là hấp và nướng. Nguyên nhân chủ yếu là do nhiệt độ, nhiệt độ quá cao làm cho hỗn hợp trứng sữa sôi lên khi chưa đông tạo nên những bọt bóng nên làm cho bánh flan bị rỗ đáy lẫn bên trong. Bên cạnh đó nếu bạn sử dụng những khuôn kim loại đi kèm với lò nướng thì khả năng bị rỗ sẽ cao hơn nếu bạn không điều chỉnh được nhiệt độ hợp lý, do khuôn kim loại sẽ dẫn nhiệt tốt hơn khuôn sứ khi nướng thì đáy khuôn sẽ tiếp xúc trực tiếp với khay nướng nên nhiệt độ sẽ rất cao.'
+            ]
+        },
+        fix: {
+            title: 'Cách khắc phục',
+            detail: [
+                'Lược hỗn hợp qua rây nhiều lần để bỏ hết bọt khí và lợn cợn của trứng',
+                'Nếu như bạn nướng bánh với khuôn kim loại thì hãy lót một cái khăn dưới đáy khây cho bánh vào và đổ nước vào khay cho ngập ½ khuôn bánh. Lưu ý nhớ làm nóng lò trước 10 phút để cho nhiệt độ ổn định rồi mới để bánh flan vào nướng .',
+            ]
+        },
+        note: {
+            title: 'Lưu ý khi áp dụng mẹo làm bánh flan không bị rỗ',
+            detail: [
+                'Đánh trứng, lọc hỗn hợp trứng qua rây: khi đánh trứng bạn không nên đánh mạnh tay tạo những bọt khí sẽ làm cho bánh khi nướng sẽ bị rỗ. Nên khuấy thật nhẹ tay và theo một chiều nhất định để trứng được tan ra. Nếu bạn sử dụng phới lòng hay dụng cụ đánh trứng thì hãy áp sát đáy tô như vậy sẽ không làm bọt khí nổi lên.',
+                'Lược hỗn hợp trứng sữa qua rây nhiều lần để loại bỏ bọt khí và lợn cợn. Lược cho thật mịn rồi mới đổ vào khuôn.',
+                'Nướng cách thủy – hấp bánh',
+                'Nướng cách thủy: Nếu như bạn nướng bánh với khuôn kim loại thì hãy lót một miếng khăn dưới đáy khây cho bánh vào và đổ nước vào khay cho ngập ½ khuôn bánh. Lưu ý nhớ làm nóng lò trước 10 phút để cho nhiệt độ ổn định rồi mới để bánh flan vào nướng .',
+                'Hấp bánh: bạn nên dùng nồi 2 tầng hoặc xửng hấp để đáy khuôn bánh flan không tiếp xúc trực tiếp với đáy nồi vì nhiệt ở đáy nồi sẽ cao hơn nên dễ làm bánh bị rỗ.'
+            ]
+        }
+    },
+    {
+        id: 2,
+        image: 'https://bepcuana.com/wp-content/uploads/2020/11/bao-quan-banh-kem-khong-co-tu-lanh-300x200.jpg',
+        title: 'Tuyệt Chiêu Bảo Quản Bánh Kem Không Có Tủ Lạnh',
+        summary: 'Bánh kem là một trong những “món bánh” được dùng trong những bữa tiệc quan trọng như là ngày sinh hay ngày cưới ngoài ra còn những dịp khác nữa nhưng điển hình là hai ngày đó. Nhưng với một chiếc bánh kem khá lớn như vậy thì không thể nào dùng hết trong một lần được vì thế chúng ra cần phải biết cách bảo quản bánh để bánh vẫn được thơm ngon, mềm ẩm và không bị chảy kem. Vậy nên Bếp Của Na sẽ chia sẽ cho bạn một số cách bảo quản bánh kem KHÔNG CÓ tủ lạnh.',
+        recipe: {
+            title: 'Dùng lát bánh mì để bảo quản phần bánh kem dở',
+            detail: [
+                'Bánh mì lát ( bánh mì sandwich) không những là một món ăn sáng tiện lợi mà còn có nhìu công dụng  và đặc biệt được dùng để bảo quản được phần bánh kem còn đang dang dở.',
+                'Đa phần khi bánh kem mua về thì với nhiệt độ bình thường nếu bạn không bảo quản đúng cách có thể làm hỏng cấu trúc của bánh kem.',
+                'Vì thế bạn hãy áp dụng cách thực hiện vô cùng đơn giản là lấy hai miếng bánh mì sandwich, sau đó cố định vào hai bên phần bánh đã cắt dỡ dùng tăm ghim vào để giữ phần bánh mì lại. Việc này giúp cấu trúc của bánh được ổn định hơn dù không có tủ lạnh.',
+                'Nếu bạn không có bánh mì sandwich thì có thể thay thế bằng bánh mì thông thường nhưng nhớ hãy cán nó mỏng ra trước nhé.',
+            ]
+        },
+        cause: {
+            title: 'Bảo quản bánh ở nhiệt độ thấp',
+            detail: [
+                'Điều kiện tốt nhất để bảo quản bánh kem chính là khi nhiệt độ xuống thấp, vì khi đó hoạt động của vi khuẩn bị hạn chế. Do đó, việc duy trì độ tươi ngon của bánh kem vào mùa đông sẽ dễ dàng hơn vào mùa hè. Hơn thế, với đặc tính của lớp kem chính là cần nhiệt độ thấp để giữa được hình dáng cho bánh  nếu không có tủ lạnh vào mùa hè thì tốt hơn hết là hãy thưởng thức bánh trước khi kem bị chảy hết.'
+            ]
+        },
+        fix: {
+            title: 'Bảo quản bánh kem khi có tủ lạnh như thế nào?',
+            detail: [
+                'Đối với nếu muốn bảo quản bánh bằng tủ thì sẽ tiện hơn nhưng nếu không biết cách thì có thể sẽ làm bánh bị khô và kem sẽ bị cứng lại không còn mềm mại nữa, nên việc quản quản bằng tủ lạnh cũng sẽ không dễ nếu như bạn không biết cách. Bình thường nếu bảo quản bánh kem ở nhiệt độ thường thì chỉ giữ độ ngon khoang 2-5 tiếng. Nhưng nếu bảo quản ở nhiệt độ của ngăn mát tủ lạnh thì độ ngon của bánh có thể giữ được 2 – 3 ngày, thế nhưng vẫn có một khuyết điểm đó là làm cốt bánh bị khô không còn mềm mịn như ban đầu.',
+            ]
+        },
+        note: {
+            title: 'Lưu ý khi bảo quản bánh kem không có tủ lạnh và có tủ lạnh',
+            detail: [
+                'Bảo quản bánh đúng thời điểm là khi nướng bánh xong đừng nên cho bánh vào hộp ngay vì hơi nóng vẫn còn có thể làm bánh bị ỉu đi vì thế hãy để cho thật nguội rồi mới cho vào hộp đây là cách bảo quản tốt nhất khi không có tủ lạnh',
+                'Tránh để không khí xâm nhập vào bánh kem trường hợp này thì bạn nên dùng hộp hay túi bóng hoặc màng bọc thực phẩm để bọc bánh lại nếu như để không khí từ bên ngoài xâm nhập vào thì việc để bánh với nhiệt độ bình thường rất dể làm bánh hư',
+                'Mức nhiệt độ thích hợp nhất để bảo quản lạnh bánh kem là từ 2 – 8 độ C. Ở nhiệt độ lạnh, vi khuẩn không thể sinh sôi nên ảnh hưởng xấu đến chất lượng của bánh. Bên cạnh đó bánh sẽ đứng, không có hiện tượng “tươm mồ hôi” gây mất thẩm mỹ.',
+                'Không nên để bánh kem ở cạnh những thực phẩm khác, đặc biệt là những thực phẩm có mùi, tươi sống như: thịt, cá, trứng… Vì bánh kem là loại bánh ăn ngay nên nếu để ở gần các thực phẩm này sẽ bị ảnh hưởng mùi dẫn đến không ngon, kém hương vị. Bánh kem có thể được bảo quản lên tới một tháng theo cách này. Tuy nhiên cũng không nên để quá lâu sẽ làm bánh bị khô, mất ngon và bị cứng lại.',
+                'Bánh phải được bọc thật kín cẩn thận nếu không sẽ rất dễ bị lên men và nhanh hỏng.',
+            ]
+        }
+    },
+    {
+        id: 3,
+        image: 'https://bepcuana.com/wp-content/uploads/2020/10/meo-lam-banh-bong-lan-xop-300x200.jpeg',
+        title: 'Cách làm bánh bông lan xốp mềm cực đơn giản tại nhà',
+        summary: 'Bánh bông lan là một trong những loại bánh được rất nhiều người yêu thích, từ người lớn đến trẻ em cũng đều rất thích dùng. Bởi hương thơm và độ mềm XỐP của bánh làm cho người ăn có cảm giác thoải mái, thế nhưng khi làm BÁNH BÔNG LAN thì bạn hay mắc phải một vài “lỗi” là bánh không được mềm, xốp và bị khô nên bánh khi làm ra ăn sẽ bị cứng không có độ đàn hồi. Thế nên Bếp Của Na sẽ giúp bạn những cách làm bánh bông lan xốp mềm. Hãy cùng tham khảo bài viết này nhé!',
+        recipe: {
+            title: '',
+            detail: [
+                '',
+            ]
+        },
+        cause: {
+            title: '',
+            detail: [
+                ''
+            ]
+        },
+        fix: {
+            title: 'Cách bảo quản bánh bông lan xốp',
+            detail: [
+                '<span style="padding-left: 20px">Để bánh vẫn còn được ngon khi chưa dùng hết bạn có thể để bánh ở nhiệt độ phòng mát mẻ, đóng gói cẩn thận trước khi cho vào tủ lạnh bảo quản hoặc có thể thêm syrup để bánh bông lan mềm mịn vẫn giữ được độ ẩm khi dùng nhé.</span>' +
+                '</br> <span style="padding-left: 20px">Để có được chiếc bánh bông lan thơm ngon, đạt chuẩn thì những mẹo trên đây có thể giúp bạn giải quyết được các vấn đề khi làm bánh mà gặp phải. Nhiều người lo lắng khi làm bánh tại sao mình đã tìm được công thức và làm theo luôn mà vẫn không thành công.</span>',
+            ]
+        },
+        note: {
+            title: 'Các cách làm bánh bông lan xốp mềm bạn đã nên thử?',
+            detail: [
+                '<span>Chọn công thức làm bánh bông lan chuẩn </span>',
+                '<span>Chọn đúng tỷ lệ thành phần nguyên liệu </span>',
+                '<span>Đánh lòng trắng trứng đúng cách </span>',
+                '<span>Kỹ thuật trộn bột fold đúng cách </span>',
+                '<span>Nướng bánh đúng cách, nhiệt độ và thời gian </span>',
+            ]
+        }
+    },
+    {
+        id: 4,
+        image: 'https://bepcuana.com/wp-content/uploads/2020/09/vo-banh-xeo-qua-day.jpg',
+        title: 'Chia sẻ nguyên nhân và cách khắc phục bánh xèo giòn ngon',
+        summary: 'Mà để có một chiếc bánh xèo ngon thì không dể gì vì khi làm mọi người thường mắc phải nhiều nguyên nhân như: vỏ bánh xèo dày, viền bánh không giòn và mỏng, bánh bị ướt, màu sắc bánh không đều và đẹp mắt…. những lỗi này đã khiến cho bánh xèo của chúng ta không còn được giòn ngon. Sau đây Bếp Của Na sẽ mách cho bạn một số nguyên nhân và cách khắc phục bánh xèo giòn của chúng ngon hơn nhé!'+
+        '</br> Nền ẩm thực của nước Việt Nam rất phong phú và đa dạng, những món ăn ở xứ đất Việt này đã làm cho những người khi đặt chân tới đây không thể nào quên được hương vị của món ăn nơi này, khiến họ phải nhất định tới đây để thưởng thức một lần nữa.'+
+        '</br>Bánh xèo, là một trong những số đó. Tuy nhìn bên ngoài có vẻ vô cùng đơn giản nhưng bên trong lại mang một hương vị khó quên. Không những chỉ có hương vị mà cái tên cũng làm cho người khác tò mò mà tìm đến để thưởng thức.',
+        recipe: {
+            title: 'Nguyên nhân',
+            detail: [
+                'Vỏ bánh quá dày không lan đều',
+                'Vỏ bánh loang lổ, không mịn màng',
+                'Bánh không có viền, hoặc có viền không được mỏng',
+                'Bị ướt sau khi chín',
+                'Bánh nhanh mềm, không giòn lâu',
+                'Những lỗi khi pha bột với nước cốt dừa',
+            ]
+        },
+        cause: {
+            title: '',
+            detail: [
+                '',
+            ]
+        },
+        fix: {
+            title: '',
+            detail: [
+                '',
+            ]
+        },
+        note: {
+            title: 'Cách khắc phục',
+            detail: [
+                'Luôn chọn dừa tươi, mới nạo hoặc bột cốt dừa tự nhiên để có mùi vị thơm ngon.',
+                'Cho một lượng bột vừa phải, thông thường là một vá canh (70ml – 90ml tùy vào độ rộng chảo từ 30cm ~ 45cm).',
+                'Chỉ cần bạn để cho phần nhân của mình ráo nước sau khi xào trước khi tráng bánh là được nhé.',
+                'Khi bột của bạn đã đủ độ loãng cần thiết, chỉ cần đổ bột, nghiêng và xoay chảo đều tay, để bột loang dần từ lòng chảo ra từ từ đến hết bột, chắc chắn bạn sẽ có được một chiếc bánh xèo với lớp viền giòn tan hoàn hảo',
+                'Ngược lại với bánh xèo bị dày, khi gặp trường hợp này, hãy thêm từ từ bột bánh vào hỗn hợp đến khi đạt được độ sệt cần thiết.',
+                'Hãy lựa chọn loại bột bánh xèo phù hợp và pha bột với tỉ lệ chuẩn để có được vỏ bánh hoàn hảo nhất. Theo chia sẻ của nhiều chị em nội trợ, khi pha bột có thể cho thêm 1 quả trứng gà hoặc thay 100 ml nước bằng 100 ml bia sẽ làm bánh giòn lâu hơn.',
+            ]
+        }
+    },
+]
